6036f0bdc4b98b89dfe8bd0845512f4113e14a6e
Afshan Akhtar <akhtaraf@sheridancollege.ca>
2023-06-04 01:36:57 +0530

    initial commit

---
+++ src/main/java/sheridan/akhtaraf/assignment1/PickABoxApplication.java
@@ -0,0 +1,13 @@
+package sheridan.akhtaraf.assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickABoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickABoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/akhtaraf/assignment1/PickABoxApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.akhtaraf.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickABoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
